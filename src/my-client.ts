daa9c4ae7da38d0b81fe07c27f1c8a752882821f
mbluteau44 <60935873+mbluteau44@users.noreply.github.com>
2023-04-14 10:17:41 -0400

    Update my-client.ts

--- src/my-client.ts
+++ src/my-client.ts
@@ -21,6 +21,7 @@ export class MyClient {
         this.remoteSupport = config?.remoteSupport
     }
 
+    // Retrieve Remote Support checkbox from Connector config(true or false).  Currently not being used in code.
     async getProduct(): Promise<any> {
 
         return this.remoteSupport
@@ -28,45 +29,43 @@ export class MyClient {
 
 
     async getAllAccounts(): Promise<any[]> {
-        let res = await sra_auth(this.authUrl,this.client_id,this.client_secret)
+        let resAuth = await sra_auth(this.authUrl,this.client_id,this.client_secret)
 
-        let resA = await sra_GET_accounts(this.instance,"Bearer "+res.data.access_token)
+        let resAccounts = await sra_GET_accounts(this.instance,"Bearer "+resAuth.data.access_token)
 
-        let resB = await sra_GET_group_policies(this.instance,"Bearer "+res.data.access_token)
+        let resGP = await sra_GET_group_policies(this.instance,"Bearer "+resAuth.data.access_token)
 
-        let GPTable = await sra_GET_account_groups_table(this.instance,"Bearer "+res.data.access_token,resB.data)
+        let GPTable = await sra_GET_account_groups_table(this.instance,"Bearer "+resAuth.data.access_token,resGP.data)
         logger.info('GPTable : '+JSON.stringify(GPTable))
 
     // GET Security Providers
-    let SPs = await sra_GET_security_providers(this.instance,"Bearer "+res.data.access_token)
+    let SPs = await sra_GET_security_providers(this.instance,"Bearer "+resAuth.data.access_token)
     logger.info('SPs.data.length = '+SPs.data.length)
 
         logger.info('getAllAccounts remoteSupport = '+this.remoteSupport)
         const ret = []
 
-        for (let index = 0; index < resA.data.length && (index) < resA.data.length; ++index) {
-//            let resC = await sra_GET_account_groups(this.instance,"Bearer "+res.data.access_token,resA.data[index].id,resB.data)
-            let resC = await sra_GET_account_groups_with_table(resA.data[index].id,GPTable)
+        for (let index = 0; index < resAccounts.data.length && (index) < resAccounts.data.length; ++index) {
+            let GroupMemberships = await sra_GET_account_groups_with_table(resAccounts.data[index].id,GPTable)
             let spname = ''
             for (let indexSP = 0; indexSP < SPs.data.length && (indexSP) < SPs.data.length; ++indexSP) {
-//                logger.info('SPs.data[indexSP].id = '+SPs.data[indexSP].id+'    security_provider_id = '+resA.data[index].security_provider_id)
-                if(SPs.data[indexSP].id == resA.data[index].security_provider_id){spname = SPs.data[indexSP].name}
+                if(SPs.data[indexSP].id == resAccounts.data[index].security_provider_id){spname = SPs.data[indexSP].name}
             }
 
             let user = {}
             user = { 
-                id: resA.data[index].id.toString(),
-                username: resA.data[index].username,
-                email_address: resA.data[index].email_address,
-                enabled: resA.data[index].enabled,
-                preferred_email_language: resA.data[index].preferred_email_language,
-                public_display_name: resA.data[index].public_display_name,
-                private_display_name: resA.data[index].private_display_name,
-                failed_logins: resA.data[index].failed_logins,
-                two_factor_required: resA.data[index].two_factor_required,
-                security_provider_id: resA.data[index].security_provider_id,
+                id: resAccounts.data[index].id.toString(),
+                username: resAccounts.data[index].username,
+                email_address: resAccounts.data[index].email_address,
+                enabled: resAccounts.data[index].enabled,
+                preferred_email_language: resAccounts.data[index].preferred_email_language,
+                public_display_name: resAccounts.data[index].public_display_name,
+                private_display_name: resAccounts.data[index].private_display_name,
+                failed_logins: resAccounts.data[index].failed_logins,
+                two_factor_required: resAccounts.data[index].two_factor_required,
+                security_provider_id: resAccounts.data[index].security_provider_id,
                 security_provider_name: spname,
-                groups: resC
+                groups: GroupMemberships
             }
             ret.push(user)
         }       
@@ -77,173 +76,170 @@ export class MyClient {
     async getAccount(identity: string): Promise<any> {
         // In a real use case, this requires a HTTP call out to SaaS app to fetch an account,
         // which is why it's good practice for this to be async and return a promise.
-        let res = await sra_auth(this.authUrl,this.client_id,this.client_secret)
+        let resAuth = await sra_auth(this.authUrl,this.client_id,this.client_secret)
 
-        let resA = await sra_GET_account(this.instance,"Bearer "+res.data.access_token,identity)
-        let resB = await sra_GET_group_policies(this.instance,"Bearer "+res.data.access_token)
+        let resAccount = await sra_GET_account(this.instance,"Bearer "+resAuth.data.access_token,identity)
+        let resGP = await sra_GET_group_policies(this.instance,"Bearer "+resAuth.data.access_token)
 
 //        let resC = await sra_GET_account_groups(this.instance,"Bearer "+res.data.access_token,resA.data.id,resB.data)
 // GET Group Policy members Table
-let GPTable = await sra_GET_account_groups_table(this.instance,"Bearer "+res.data.access_token,resB.data)
+let GPTable = await sra_GET_account_groups_table(this.instance,"Bearer "+resAuth.data.access_token,resGP.data)
 // GET Security Providers
-    let SPs = await sra_GET_security_providers(this.instance,"Bearer "+res.data.access_token)
-let resC = await sra_GET_account_groups_with_table(resA.data.id,GPTable)
+    let SPs = await sra_GET_security_providers(this.instance,"Bearer "+resAuth.data.access_token)
+let GroupMemberships = await sra_GET_account_groups_with_table(resAccount.data.id,GPTable)
     let spname = ''
     for (let indexSP = 0; indexSP < SPs.data.length && (indexSP) < SPs.data.length; ++indexSP) {
-        if(SPs.data[indexSP].id == resA.data.security_provider_id){spname = SPs.data[indexSP].name}
+        if(SPs.data[indexSP].id == resAccount.data.security_provider_id){spname = SPs.data[indexSP].name}
     }
 
         return { 
-                id: resA.data.id.toString(),
-                username: resA.data.username,
-                email_address: resA.data.email_address,
-                enable: resA.data.enabled,
-                preferred_email_language: resA.data.preferred_email_language,
-                public_display_name: resA.data.public_display_name,
-                private_display_name: resA.data.private_display_name,
-                failed_logins: resA.data.failed_logins,
-                two_factor_required: resA.data.two_factor_required,
-                security_provider_id: resA.data.security_provider_id,
+                id: resAccount.data.id.toString(),
+                username: resAccount.data.username,
+                email_address: resAccount.data.email_address,
+                enable: resAccount.data.enabled,
+                preferred_email_language: resAccount.data.preferred_email_language,
+                public_display_name: resAccount.data.public_display_name,
+                private_display_name: resAccount.data.private_display_name,
+                failed_logins: resAccount.data.failed_logins,
+                two_factor_required: resAccount.data.two_factor_required,
+                security_provider_id: resAccount.data.security_provider_id,
                 security_provider_name: spname,
-                groups: resC
+                groups: GroupMemberships
             }
     }
 
     async createAccount(account: string): Promise<any> {
         // In a real use case, this requires a HTTP call out to SaaS app to fetch an account,
         // which is why it's good practice for this to be async and return a promise.
-        let res = await sra_auth(this.authUrl,this.client_id,this.client_secret)
+        let resAuth = await sra_auth(this.authUrl,this.client_id,this.client_secret)
 
-        let resA: any = await sra_create_account_ent(this.instance,this.remoteSupport,"Bearer "+res.data.access_token,account)
-        logger.info(`myClient resA : ${JSON.stringify(resA)}`)
+        let resAccount: any = await sra_create_account_ent(this.instance,this.remoteSupport,"Bearer "+resAuth.data.access_token,account)
+        logger.info(`myClient resA : ${JSON.stringify(resAccount)}`)
         logger.info(`remoteSupport : ${this.remoteSupport}`)
-        let resB = await sra_GET_group_policies(this.instance,"Bearer "+res.data.access_token)
-//        let resC = await sra_GET_account_groups(this.instance,"Bearer "+res.data.access_token,resA.id,resB.data)
+        let resGP = await sra_GET_group_policies(this.instance,"Bearer "+resAuth.data.access_token)
 // GET Group Policy members Table
-let GPTable = await sra_GET_account_groups_table(this.instance,"Bearer "+res.data.access_token,resB.data)
+let GPTable = await sra_GET_account_groups_table(this.instance,"Bearer "+resAuth.data.access_token,resGP.data)
 // GET Security Providers
-    let SPs = await sra_GET_security_providers(this.instance,"Bearer "+res.data.access_token)
-let resC = await sra_GET_account_groups_with_table(resA.id,GPTable)
+    let SPs = await sra_GET_security_providers(this.instance,"Bearer "+resAuth.data.access_token)
+let GroupMemberships = await sra_GET_account_groups_with_table(resAccount.id,GPTable)
     let spname = ''
     for (let indexSP = 0; indexSP < SPs.data.length && (indexSP) < SPs.data.length; ++indexSP) {
-        if(SPs.data[indexSP].id == resA.security_provider_id){spname = SPs.data[indexSP].name}
+        if(SPs.data[indexSP].id == resAccount.security_provider_id){spname = SPs.data[indexSP].name}
     }
 
         return { 
-                id: resA.id.toString(),
-                username: resA.username,
-                email_address: resA.email_address,
-                enable: resA.enabled,
-                preferred_email_language: resA.preferred_email_language,
-                public_display_name: resA.public_display_name,
-                private_display_name: resA.private_display_name,
-                failed_logins: resA.failed_logins,
-                two_factor_required: resA.two_factor_required,
-                security_provider_id: resA.security_provider_id,
+                id: resAccount.id.toString(),
+                username: resAccount.username,
+                email_address: resAccount.email_address,
+                enable: resAccount.enabled,
+                preferred_email_language: resAccount.preferred_email_language,
+                public_display_name: resAccount.public_display_name,
+                private_display_name: resAccount.private_display_name,
+                failed_logins: resAccount.failed_logins,
+                two_factor_required: resAccount.two_factor_required,
+                security_provider_id: resAccount.security_provider_id,
                 security_provider_name: spname,
-                groups: resC
+                groups: GroupMemberships
             }
     }
 
     async changeAccount(account: string, change: any): Promise<any> {
         // In a real use case, this requires a HTTP call out to SaaS app to fetch an account,
         // which is why it's good practice for this to be async and return a promise.
-        let res = await sra_auth(this.authUrl,this.client_id,this.client_secret)
+        let resAuth = await sra_auth(this.authUrl,this.client_id,this.client_secret)
 
 //        throw new ConnectorError('Unknown account change op: ' + JSON.stringify(changes))        
 
 //        changes.forEach((c: any) => {
-            sra_change_account(this.instance,"Bearer "+res.data.access_token,account,change)
+            sra_change_account(this.instance,"Bearer "+resAuth.data.access_token,account,change)
             logger.info(`forEach change : ${JSON.stringify(change)}`)
 //        })
 
-        let resB = await sra_GET_group_policies(this.instance,"Bearer "+res.data.access_token)
-//        let resC = await sra_GET_account_groups(this.instance,"Bearer "+res.data.access_token,resA.data.id,resB.data)
+        let resGP = await sra_GET_group_policies(this.instance,"Bearer "+resAuth.data.access_token)
 // GET Group Policy members Table
-let GPTable = await sra_GET_account_groups_table(this.instance,"Bearer "+res.data.access_token,resB.data)
+let GPTable = await sra_GET_account_groups_table(this.instance,"Bearer "+resAuth.data.access_token,resGP.data)
 // GET Security Providers
-    let SPs = await sra_GET_security_providers(this.instance,"Bearer "+res.data.access_token)
-    let resA = await sra_GET_account(this.instance,"Bearer "+res.data.access_token,account)
-    let resC = await sra_GET_account_groups_with_table(resA.data.id,GPTable)
+    let SPs = await sra_GET_security_providers(this.instance,"Bearer "+resAuth.data.access_token)
+    let resAccount = await sra_GET_account(this.instance,"Bearer "+resAuth.data.access_token,account)
+    let GroupMemberships = await sra_GET_account_groups_with_table(resAccount.data.id,GPTable)
     let spname = ''
     for (let indexSP = 0; indexSP < SPs.data.length && (indexSP) < SPs.data.length; ++indexSP) {
-        if(SPs.data[indexSP].id == resA.data.security_provider_id){spname = SPs.data[indexSP].name}
+        if(SPs.data[indexSP].id == resAccount.data.security_provider_id){spname = SPs.data[indexSP].name}
     }
         
         return { 
-                id: resA.data.id.toString(),
-                username: resA.data.username,
-                email_address: resA.data.email_address,
-                enable: resA.data.enabled,
-                preferred_email_language: resA.data.preferred_email_language,
-                public_display_name: resA.data.public_display_name,
-                private_display_name: resA.data.private_display_name,
-                failed_logins: resA.data.failed_logins,
-                two_factor_required: resA.data.two_factor_required,
-                security_provider_id: resA.data.security_provider_id,
+                id: resAccount.data.id.toString(),
+                username: resAccount.data.username,
+                email_address: resAccount.data.email_address,
+                enable: resAccount.data.enabled,
+                preferred_email_language: resAccount.data.preferred_email_language,
+                public_display_name: resAccount.data.public_display_name,
+                private_display_name: resAccount.data.private_display_name,
+                failed_logins: resAccount.data.failed_logins,
+                two_factor_required: resAccount.data.two_factor_required,
+                security_provider_id: resAccount.data.security_provider_id,
                 security_provider_name: spname,
-                groups: resC
+                groups: GroupMemberships
             }
     }
 
     async changeAccountStatus(account: string, change: any): Promise<any> {
         // In a real use case, this requires a HTTP call out to SaaS app to fetch an account,
         // which is why it's good practice for this to be async and return a promise.
-        let res = await sra_auth(this.authUrl,this.client_id,this.client_secret)
+        let resAuth = await sra_auth(this.authUrl,this.client_id,this.client_secret)
   
-        let resB = await sra_GET_group_policies(this.instance,"Bearer "+res.data.access_token)
-//        let resC = await sra_GET_account_groups(this.instance,"Bearer "+res.data.access_token,resA.data.id,resB.data)
+        let resGP = await sra_GET_group_policies(this.instance,"Bearer "+resAuth.data.access_token)
 // GET Group Policy members Table
-let GPTable = await sra_GET_account_groups_table(this.instance,"Bearer "+res.data.access_token,resB.data)
+let GPTable = await sra_GET_account_groups_table(this.instance,"Bearer "+resAuth.data.access_token,resGP.data)
 // GET Security Providers
-    let SPs = await sra_GET_security_providers(this.instance,"Bearer "+res.data.access_token)
-    let resA = await sra_change_account_status(this.instance,"Bearer "+res.data.access_token,account,change)
-    let resC = await sra_GET_account_groups_with_table(resA.data.id,GPTable)
+    let SPs = await sra_GET_security_providers(this.instance,"Bearer "+resAuth.data.access_token)
+    let resAccount = await sra_change_account_status(this.instance,"Bearer "+resAuth.data.access_token,account,change)
+    let GroupMemberships = await sra_GET_account_groups_with_table(resAccount.data.id,GPTable)
     let spname = ''
     for (let indexSP = 0; indexSP < SPs.data.length && (indexSP) < SPs.data.length; ++indexSP) {
-        if(SPs.data[indexSP].id == resA.data.security_provider_id){spname = SPs.data[indexSP].name}
+        if(SPs.data[indexSP].id == resAccount.data.security_provider_id){spname = SPs.data[indexSP].name}
     }
         
         return { 
-                id: resA.data.id.toString(),
-                username: resA.data.username,
-                email_address: resA.data.email_address,
-                enable: resA.data.enabled,
-                preferred_email_language: resA.data.preferred_email_language,
-                public_display_name: resA.data.public_display_name,
-                private_display_name: resA.data.private_display_name,
-                failed_logins: resA.data.failed_logins,
-                two_factor_required: resA.data.two_factor_required,
-                security_provider_id: resA.data.security_provider_id,
+                id: resAccount.data.id.toString(),
+                username: resAccount.data.username,
+                email_address: resAccount.data.email_address,
+                enable: resAccount.data.enabled,
+                preferred_email_language: resAccount.data.preferred_email_language,
+                public_display_name: resAccount.data.public_display_name,
+                private_display_name: resAccount.data.private_display_name,
+                failed_logins: resAccount.data.failed_logins,
+                two_factor_required: resAccount.data.two_factor_required,
+                security_provider_id: resAccount.data.security_provider_id,
                 security_provider_name: spname,
-                groups: resC
+                groups: GroupMemberships
             }
     }
 
     async testConnection(): Promise<any> {
 
-        let res = await sra_auth(this.authUrl,this.client_id,this.client_secret)
+        let resAuth = await sra_auth(this.authUrl,this.client_id,this.client_secret)
 
 
-logger.info(`res : ${res}`)
+logger.info(`resAuth : ${resAuth}`)
 
 return {}
 }
 async getAllEntitlements(): Promise<any[]> {
-    let res = await sra_auth(this.authUrl,this.client_id,this.client_secret)
+    let resAuth = await sra_auth(this.authUrl,this.client_id,this.client_secret)
 
-    let resA = await sra_GET_group_policies(this.instance,"Bearer "+res.data.access_token)
+    let resGP = await sra_GET_group_policies(this.instance,"Bearer "+resAuth.data.access_token)
 
-    return resA.data
+    return resGP.data
 
 }
 
 async getEntitlement(identity: string): Promise<any[]> {
-    let res = await sra_auth(this.authUrl,this.client_id,this.client_secret)
+    let resAuth = await sra_auth(this.authUrl,this.client_id,this.client_secret)
 
-    let resA = await sra_GET_group_policy(this.instance,"Bearer "+res.data.access_token,identity)
+    let resGP = await sra_GET_group_policy(this.instance,"Bearer "+resAuth.data.access_token,identity)
 
-    return resA.data
+    return resGP.data
 
 }
 
